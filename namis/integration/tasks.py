dd15794a1e92049d2099853bdafd5b38621398d5
Brian Simpokolwe <briansimpokolwe@gmail.com>
2024-08-08 13:01:52 +0200

    dev

--- namis/integration/tasks.py
+++ namis/integration/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 from .services import Processor
 
-@shared_task
+@shared_task(soft_time_limit=72000, time_limit=324000)  # 30days, 90days
 def  post_file(filepath):
     try:
         processor = Processor()
