42f60c5108fa4cccc84c87876cae68dbb70350c8
Brian Simpokolwe <briansimpokolwe@gmail.com>
2024-08-08 18:29:52 +0200

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,7 +50,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": env("POSTGRES_HOST"),
         "PORT": env("POSTGRES_PORT"),
-        "NAME": env("POSTGRES_DB")
+        "NAME": env("POSTGRES_DB"),
         "USER": env("POSTGRES_USER"),
         "PASSWORD": env("POSTGRES_PASSWORD")
     }
